35555e2a96f8193b74456946ab93536cd9feff91
Dilnavazsida <navazdil07@gmail.com>
2025-03-27 23:20:31 +0530

    Add files via upload

---
+++ con_to_uper.py
@@ -0,0 +1,36 @@
+        #   write a function that convert a string into uppercase
+
+            # with using upper function
+
+# def upper_case(str1):
+#     return  str1.upper()
+# print(upper_case("write a function that convert a string into uppercase"))
+
+
+            # with using ascii values
+# converting to upper case
+# def str_upper(s):
+#     uper =""
+
+#     for i in s:
+#         ascii = ord(i)
+#         if 97 <= ascii <=122:
+#             uper += chr(ascii-32)
+#         else:
+#             uper+=chr(ascii)
+#     return uper
+# print(str_upper("bismillah"))
+
+
+           # converting to lower case
+
+# def lower(s):
+#     uper=""
+#     for i in s:
+#         ascii = ord(i)
+#         if 65 <= ascii <= 91:
+#             uper+=chr(ascii+32)
+#         else:
+#             uper+=chr(ascii)
+#     return uper
+# print(lower("BISMILLAH"))
