3d72f2e8295fb4a0d6d794e6329e2ba3abe0abe1
Dilnavazsida <navazdil07@gmail.com>
2025-07-03 23:03:26 +0530

    Add files via upload

---
+++ prime_num.py
@@ -0,0 +1,22 @@
+# user = int(input("enter the number to check it's prime or not"))
+
+# if user <=1:
+#     print("not prime")
+# else:
+
+#     for i in range(2,user):
+#         if user % i == 0:
+#             print("number not prime")
+#             break
+#     else:
+#         print("number is prime")
+
+
+
+
+
+
+
+
+
+
