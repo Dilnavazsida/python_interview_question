be1bb6214115b0f68bee33f75b21c155cabc3264
Dilnavazsida <navazdil07@gmail.com>
2025-07-04 23:47:42 +0530

    Add files via upload

---
+++ swap_num.py
@@ -0,0 +1,16 @@
+a = int(input("enter your first number ")) # 10
+b = int(input("enter your second number ")) # 20
+
+a = a+b  # 10+20 = 30
+b = a-b  #  30-20 = 10
+a = a-b  #  30-10 = 20
+
+print(f" A = {a} , B = {b}")
+
+# second way
+
+a = a*b  # 10*20 = 200
+b = a/b  #  200/20 = 10
+a = a/b  #  200/10 = 20
+
+print(f" A = {int(a)} , B = {int(b)}")
