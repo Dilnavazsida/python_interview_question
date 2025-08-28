0f2faccbefa11f15603211b37eb1d8f9ea05c961
Dilnavazsida <navazdil07@gmail.com>
2025-08-28 23:56:13 +0530

    Update permutation.py

--- permutation.py
+++ permutation.py
@@ -1,3 +1,23 @@
+def permutations(lst):
+    if len(lst) == 1:
+        return [lst]
+      
+    result = []
+
+    for i in range(len(lst)):
+        current = lst[i]
+        remaining = lst[:i] + lst[i+1:]
+
+        for perm in permutations(remaining):
+            result.append([current] + perm)
+
+    return result
+  
+print(permutations([1,2,3]))
+
+
+# Another Way 
+
 from itertools import permutations
 
 l1 = [1,2,3]
